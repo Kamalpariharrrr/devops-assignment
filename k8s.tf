d5ecc36e2c3c962dd8b861b41f865d4b0bcb6e67
Kamalpariharrrr <kparihar015@gmail.com>
2024-12-11 20:46:02 +0530

    initial commit

---
+++ k8s.tf
@@ -0,0 +1,78 @@
+resource "kubernetes_namespace" "devops-assignment" {
+  metadata {
+    name = "k8s-ns-by-tf"
+  }
+}
+
+resource "kubernetes_deployment" "devops-assignment" {
+  metadata {
+    name = "terraform-devops-assignment"
+    labels = {
+      test = "DevopsAssignment"
+    }
+    namespace = kubernetes_namespace.devops-assignment.metadata[0].name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "DevopsAssignment"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "DevopsAssignment"
+        }
+      }
+
+      spec {
+        container {
+          image = "kmlparihar14/devops-assignment:latest"
+          name  = "devops-assignment"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "devops-assignment" {
+  metadata {
+    name      = "devops-assignment-service"
+    namespace = kubernetes_namespace.devops-assignment.metadata[0].name
+    labels = {
+      test = "DevopsAssignment"
+    }
+  }
+
+  spec {
+    selector = {
+      test = "DevopsAssignment"
+    }
+
+    port {
+      port        = 3000
+      target_port = 3000
+      protocol    = "TCP"
+      node_port   = 30000 # Optional: Specify the exact NodePort
+    }
+    
+    type = "NodePort"
+
+    external_traffic_policy = "Local" 
+  }
+}
